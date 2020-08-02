90e3bbc22bec8d6d138a1f69121f89b9a108dffa
RaddedMC <mycooljames@hotmail.com>
2020-08-02 16:55:34 -0400

    Photo generater + cli arguments working!

--- SmartFramePhotoGen.py
+++ SmartFramePhotoGen.py
@@ -1,3 +1,5 @@
+#!/bin/env python
+
 # Get data from:
 	# Spotify
 	# Google Cast
@@ -15,28 +17,55 @@
 
 # -- IMPORTS --
 import sys
+from PIL import Image
 
-def throwErr(msg, code):
+
+# -- BASIC FUNCTIONS --
+def throwErr(msg, code = 5):
 	print("Error: " + msg)
+	print("SmartFramePhotoGen -h for help.")
 	exit(code)
 
-def throwErr(msg):
-	throwErr(msg, 5)	
+def isThisANumber(isit):
+	if isinstance(isit, (int, float, complex)) and not isinstance(isit, bool):
+		return True
+	else:
+		return False
+
+def getHelp():
+	print("You've called the help function. Unfortunately, there's nothing here right now.")
+	exit(0)
+	#TODO: ADD MORE TO THIS
+
+
+# -- GENPHOTO --
+def genPhoto(xRes, yRes):
+	image = Image.new('RGB', (xRes, yRes))
+	# Data code here...
+	image.save("SmartFrame.png")
+
 
 # -- MAIN --
 def main():
-	# Help, Resolution
 	argc = len(sys.argv)
+
 	if argc > 1:
 		if sys.argv[1] == "-h" or sys.argv[1] == "--help":
-			print("HELP!")
-			exit()
+			getHelp()
 		if argc > 2:
-			print("Resolution set: " + sys.argv[1] + "x" + sys.argv[2])
+			try:
+				xRes = int(sys.argv[1])
+				yRes = int(sys.argv[2])
+			except ValueError:
+				throwErr("given argument is not a number!")
+			print("Resolution set: " + str(xRes) + "x" + str(yRes))
+			genPhoto(xRes, yRes)
 		else:
 			throwErr("Unrecognized argument.")
+
 	else:
 		print("Assuming a default resolution of 1280x800:")
+		genPhoto(1280, 800)
 
 if __name__ == "__main__":
 	main()
